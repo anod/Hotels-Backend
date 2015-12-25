54db529ea8be67dca85fce942a0fa3e51a058f60
Alex Gavrishev <alex.gavrishev@gmail.com>
2015-12-25 10:17:16 +0200

    add initial project

---
+++ HotelsWizard/Controllers/SearchController.cs
@@ -0,0 +1,15 @@
+﻿using System.Web.Http;
+using Microsoft.Azure.Mobile.Server.Config;
+
+namespace HotelsWizard.Controllers
+{
+    [MobileAppController]
+    public class SearchController : ApiController
+    {
+        // GET api/Look
+        public string Get()
+        {
+            return "Hello from custom controller!";
+        }
+    }
+}
